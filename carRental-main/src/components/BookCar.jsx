d56b421b28f0cc9f0bc1edc2a2e5397da817a8eb
proprathamesh <prathameshsyadav1712@gmail.com>
2024-08-18 23:44:54 +0530

    first commit

---
+++ carRental-main/src/components/BookCar.jsx
@@ -0,0 +1,476 @@
+import { useEffect, useState } from "react";
+import emailjs from 'emailjs-com';
+import { CAR_DATA } from "./CarData";
+
+function BookCar() {
+
+  emailjs.init("qznwuddWC-P50vO02");
+  const [modal, setModal] = useState(false); //  class - active-modal
+
+  // booking car
+  const [carType, setCarType] = useState(0);
+  const [carName, setCarName] = useState(CAR_DATA[0][0].name);
+  const [pickUp, setPickUp] = useState("");
+  const [dropOff, setDropOff] = useState("");
+  const [pickTime, setPickTime] = useState("");
+  const [dropTime, setDropTime] = useState("");
+  const [carImg, setCarImg] = useState("");
+
+  // modal infos
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [zipcode, setZipCode] = useState("");
+
+  useEffect(() => {
+    setCarName(CAR_DATA[carType][0].name)
+    setCarImg(CAR_DATA[carType][0].img)
+  }, [carType])
+
+  // taking value of modal inputs
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleLastName = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const handlePhone = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const handleAge = (e) => {
+    setAge(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleAddress = (e) => {
+    setAddress(e.target.value);
+  };
+
+  const handleCity = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleZip = (e) => {
+    setZipCode(e.target.value);
+  };
+
+  // open modal when all inputs are fulfilled
+  const openModal = (e) => {
+    e.preventDefault();
+    console.log(pickUp)
+    console.log(dropOff)
+    console.log(pickTime)
+    console.log(dropTime)
+    console.log(carType)
+    const errorMsg = document.querySelector(".error-message");
+    if (
+      pickUp === "" ||
+      dropOff === "" ||
+      pickTime === "" ||
+      dropTime === "" ||
+      carType === ""
+    ) {
+      errorMsg.style.display = "flex";
+    } else {
+      setModal(!modal);
+      const modalDiv = document.querySelector(".booking-modal");
+      modalDiv.scroll(0, 0);
+      errorMsg.style.display = "none";
+    }
+  };
+
+  // disable page scroll when modal is displayed
+  useEffect(() => {
+    if (modal === true) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [modal]);
+
+  // confirm modal booking
+  const confirmBooking = (e) => {
+    e.preventDefault();
+    setModal(!modal);
+    const doneMsg = document.querySelector(".booking-done");
+    doneMsg.style.display = "flex";
+
+    console.log({
+      name,
+      to_email: email,
+      car_name: carName,
+      pick_up: pickUp,
+      drop_off: dropOff,
+      pick_time: pickTime,
+      drop_time: dropTime,
+      address
+    });
+
+    emailjs.send('service_6lers3f', 'template_4934xif', {
+      name: name,                     // Sender's name
+      to_email: email,                // Recipient's email address
+      car_name: carName,              // Car name
+      pick_up: pickUp,                // Pick-up location
+      drop_off: dropOff,              // Drop-off location
+      pick_time: pickTime,            // Pick-up time
+      drop_time: dropTime,            // Drop-off time
+      address: address,  
+    })
+    .then((response) => {
+      console.log('Email sent successfully:', response);
+    })
+    .catch((error) => {
+      console.error('Error sending email:', error);
+    });
+  };
+
+  // taking value of booking inputs
+  const handleCar = (e) => {
+    setCarType(e.target.value);
+  };
+
+  const handlePick = (e) => {
+    setPickUp(e.target.value);
+  };
+
+  const handleDrop = (e) => {
+    setDropOff(e.target.value);
+  };
+
+  const handlePickTime = (e) => {
+    setPickTime(e.target.value);
+  };
+
+  const handleDropTime = (e) => {
+    setDropTime(e.target.value);
+  };
+
+  // hide message
+  const hideMessage = () => {
+    const doneMsg = document.querySelector(".booking-done");
+    doneMsg.style.display = "none";
+  };
+
+  return (
+    <>
+      <section id="booking-section" className="book-section">
+        {/* overlay */}
+        <div
+          onClick={openModal}
+          className={`modal-overlay ${modal ? "active-modal" : ""}`}
+        ></div>
+
+        <div className="container">
+          <div className="book-content">
+            <div className="book-content__box">
+              <h2>Book a car</h2>
+
+              <p className="error-message">
+                All fields required! <i className="fa-solid fa-xmark"></i>
+              </p>
+
+              <p className="booking-done">
+                Check your email to confirm an order.{" "}
+                <i onClick={hideMessage} className="fa-solid fa-xmark"></i>
+              </p>
+
+              <form className="box-form">
+                <div className="box-form__car-type">
+                  <label>
+                    <i className="fa-solid fa-car"></i> &nbsp; Select Your Car
+                    Type <b>*</b>
+                  </label>
+                  <select value={carType} onChange={handleCar}>
+                    {CAR_DATA.flat().map((car) => (
+                      <option key={car.id} value={car.id}>
+                        {car.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+
+                <div className="box-form__car-type">
+                  <label>
+                    <i className="fa-solid fa-location-dot"></i> &nbsp; Pick-up{" "}
+                    <b>*</b>
+                  </label>
+                  <input placeholder="Enter pickup location" value={pickUp} onChange={handlePick} style={{
+                    fontSize: '1.5rem',
+                    color: '#ababab',
+                    fontFamily: '$text-font',
+                    border: '1px solid #ccd7e6',
+                    borderRadius: '3px',
+                    fontWeight: '400',
+                    padding: '1.2rem 1.3rem',
+                    outline: 'none',
+                  }} />
+                </div>
+
+                <div className="box-form__car-type">
+                  <label>
+                    <i className="fa-solid fa-location-dot"></i> &nbsp; Drop-off{" "}
+                    <b>*</b>
+                  </label>
+                  <input placeholder="Enter drop location" value={dropOff} onChange={handleDrop} style={{
+                    fontSize: '1.5rem',
+                    color: '#ababab',
+                    fontFamily: '$text-font',
+                    border: '1px solid #ccd7e6',
+                    borderRadius: '3px',
+                    fontWeight: '400',
+                    padding: '1.2rem 1.3rem',
+                    outline: 'none',
+                  }}/>
+                </div>
+
+                <div className="box-form__car-time">
+                  <label htmlFor="picktime">
+                    <i className="fa-regular fa-calendar-days "></i> &nbsp;
+                    Pick-up <b>*</b>
+                  </label>
+                  <input
+                    id="picktime"
+                    value={pickTime}
+                    onChange={handlePickTime}
+                    type="date"
+                  ></input>
+                </div>
+
+                <div className="box-form__car-time">
+                  <label htmlFor="droptime">
+                    <i className="fa-regular fa-calendar-days "></i> &nbsp;
+                    Drop-off <b>*</b>
+                  </label>
+                  <input
+                    id="droptime"
+                    value={dropTime}
+                    onChange={handleDropTime}
+                    type="date"
+                  ></input>
+                </div>
+
+                <button onClick={openModal} type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* modal ------------------------------------ */}
+
+      <div className={`booking-modal ${modal ? "active-modal" : ""}`}>
+        {/* title */}
+        <div className="booking-modal__title">
+          <h2>Complete Reservation</h2>
+          <i onClick={openModal} className="fa-solid fa-xmark"></i>
+        </div>
+        {/* message */}
+        <div className="booking-modal__message">
+          <h4>
+            <i className="fa-solid fa-circle-info"></i> Upon completing this
+            reservation enquiry, you will receive:
+          </h4>
+          <p>
+            Your rental voucher to produce on arrival at the rental desk and a
+            toll-free customer support number.
+          </p>
+        </div>
+        {/* car info */}
+        <div className="booking-modal__car-info">
+          <div className="dates-div">
+            <div className="booking-modal__car-info__dates">
+              <h5>Location & Date</h5>
+              <span>
+                <i className="fa-solid fa-location-dot"></i>
+                <div>
+                  <h6>Pick-Up Date & Time</h6>
+                  <p>
+                    {pickTime} /{" "}
+                    <input type="time" className="input-time"></input>
+                  </p>
+                </div>
+              </span>
+            </div>
+
+            <div className="booking-modal__car-info__dates">
+              <span>
+                <i className="fa-solid fa-location-dot"></i>
+                <div>
+                  <h6>Drop-Off Date & Time</h6>
+                  <p>
+                    {dropTime} /{" "}
+                    <input type="time" className="input-time"></input>
+                  </p>
+                </div>
+              </span>
+            </div>
+
+            <div className="booking-modal__car-info__dates">
+              <span>
+                <i className="fa-solid fa-calendar-days"></i>
+                <div>
+                  <h6>Pick-Up Location</h6>
+                  <p>{pickUp}</p>
+                </div>
+              </span>
+            </div>
+
+            <div className="booking-modal__car-info__dates">
+              <span>
+                <i className="fa-solid fa-calendar-days"></i>
+                <div>
+                  <h6>Drop-Off Location</h6>
+                  <p>{dropOff}</p>
+                </div>
+              </span>
+            </div>
+          </div>
+          <div className="booking-modal__car-info__model">
+            <h5>
+              <span>Car -</span> {carName}
+            </h5>
+            {carImg && <img src={carImg} alt="car_img" />}
+          </div>
+        </div>
+        {/* personal info */}
+        <div className="booking-modal__person-info">
+          <h4>Personal Information</h4>
+          <form className="info-form">
+            <div className="info-form__2col">
+              <span>
+                <label>
+                  First Name <b>*</b>
+                </label>
+                <input
+                  value={name}
+                  onChange={handleName}
+                  type="text"
+                  placeholder="Enter your first name"
+                ></input>
+                <p className="error-modal">This field is required.</p>
+              </span>
+
+              <span>
+                <label>
+                  Last Name <b>*</b>
+                </label>
+                <input
+                  value={lastName}
+                  onChange={handleLastName}
+                  type="text"
+                  placeholder="Enter your last name"
+                ></input>
+                <p className="error-modal ">This field is required.</p>
+              </span>
+
+              <span>
+                <label>
+                  Phone Number <b>*</b>
+                </label>
+                <input
+                  value={phone}
+                  onChange={handlePhone}
+                  type="tel"
+                  placeholder="Enter your phone number"
+                ></input>
+                <p className="error-modal">This field is required.</p>
+              </span>
+
+              <span>
+                <label>
+                  Age <b>*</b>
+                </label>
+                <input
+                  value={age}
+                  onChange={handleAge}
+                  type="number"
+                  placeholder="18"
+                ></input>
+                <p className="error-modal ">This field is required.</p>
+              </span>
+            </div>
+
+            <div className="info-form__1col">
+              <span>
+                <label>
+                  Email <b>*</b>
+                </label>
+                <input
+                  value={email}
+                  onChange={handleEmail}
+                  type="email"
+                  placeholder="Enter your email address"
+                ></input>
+                <p className="error-modal">This field is required.</p>
+              </span>
+
+              <span>
+                <label>
+                  Address <b>*</b>
+                </label>
+                <input
+                  value={address}
+                  onChange={handleAddress}
+                  type="text"
+                  placeholder="Enter your street address"
+                ></input>
+                <p className="error-modal ">This field is required.</p>
+              </span>
+            </div>
+
+            <div className="info-form__2col">
+              <span>
+                <label>
+                  City <b>*</b>
+                </label>
+                <input
+                  value={city}
+                  onChange={handleCity}
+                  type="text"
+                  placeholder="Enter your city"
+                ></input>
+                <p className="error-modal">This field is required.</p>
+              </span>
+
+              <span>
+                <label>
+                  Zip Code <b>*</b>
+                </label>
+                <input
+                  value={zipcode}
+                  onChange={handleZip}
+                  type="text"
+                  placeholder="Enter your zip code"
+                ></input>
+                <p className="error-modal ">This field is required.</p>
+              </span>
+            </div>
+
+            <span className="info-form__checkbox">
+              <input type="checkbox"></input>
+              <p>Please send me latest news and updates</p>
+            </span>
+
+            <div className="reserve-button">
+              <button onClick={confirmBooking}>Reserve Now</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default BookCar;
---
+++ carRental-main/src/components/CarData.js
@@ -0,0 +1,99 @@
+import swift from "../images/cars-big/dzire.jpg";
+import wagon from "../images/cars-big/wagon.jpg";
+import ertiga from "../images/cars-big/ertiga.jpg";
+import karens from "../images/cars-big/karens.jpg";
+import innova from "../images/cars-big/Innova.jpg";
+import crysta from "../images/cars-big/crysta.png";
+
+export const CAR_DATA = [
+  [
+    {
+      id: 0,
+      name: "Wagon R",
+      price: "12",
+      img: wagon,
+      model: "Wagon R",
+      mark: "Maruti Suzuki",
+      year: "new model",
+      seats: "4+1",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel/CNG",
+    },
+  ],
+  [
+    {
+      id: 1,
+      name: "Swift Dzire",
+      price: "12",
+      img: swift,
+      model: "Dzire",
+      mark: "Maruti Suzuki",
+      year: "new model",
+      seats: "4+1",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel/CNG",
+    },
+  ],
+  [
+    {
+      id: 2,
+      name: "Ertiga",
+      price: "15",
+      img: ertiga,
+      model: "Ertiga",
+      mark: "Maruti Suzuki",
+      year: "new model",
+      seats: "6+1",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel/CNG",
+    },
+  ],
+  [
+    {
+      id: 3,
+      name: "Kia Karens",
+      price: "15",
+      img: karens,
+      model: "Karens",
+      mark: "Kia",
+      year: "new model",
+      seats: "6+1",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      id: 4,
+      name: "Innova",
+      price: "17",
+      img: innova,
+      model: "Innova",
+      mark: "Toyota",
+      year: "2016",
+      seats: "7+1",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      id: 5,
+      name: "Innova Crysta",
+      price: "19",
+      img: crysta,
+      model: "Innova",
+      mark: "Toyota",
+      year: "2018",
+      seats: "7+1",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+];
